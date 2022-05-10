45104a2d75a72407ca38b3ece292942f78706d60
KwonDae <green201402317@gmail.com>
2022-05-10 17:00:29 +0900

    [week2] Layouts in Jetpack Compose 완료

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/week2layouts_in_jetpack_compose/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/com/example/week2layouts_in_jetpack_compose/MainActivity.kt
@@ -1,43 +1,141 @@
 package com.example.week2layouts_in_jetpack_compose
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.*
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.*
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.repeatOnLifecycle
-import coil.compose.rememberImagePainter
+import androidx.compose.ui.unit.*
 import com.example.week2layouts_in_jetpack_compose.ui.theme.LayoutsCodelabTheme
-import kotlinx.coroutines.launch
+import java.time.DayOfWeek
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             LayoutsCodelabTheme {
-                ScrollingList()
+                BodyContent()
             }
         }
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+
+        // Set the size of the parent layout
+        layout(width, height) {
+            // x cord we have placed up to, per row
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(16.dp, 16.dp)
+                    .background(color = MaterialTheme.colors.secondary)
+            )
+            Spacer(Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .background(color = Color.LightGray, shape = RectangleShape)
+            .padding(16.dp)
+            .size(200.dp)
+            .horizontalScroll(rememberScrollState())
+    ) {
+        StaggeredGrid {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    LayoutsCodelabTheme {
+        BodyContent()
     }
 }
 
@@ -77,76 +175,76 @@ class MainActivity : ComponentActivity() {
 //    }
 //}
 
-@Composable
-fun ImageList() {
-    val scrollState = rememberLazyListState()
-
-    LazyColumn(state = scrollState) {
-        items(100) {
-            ImageListItem(it)
-        }
-    }
-}
-
-@Composable
-fun ImageListItem(index: Int) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
-        Image(
-            painter = rememberImagePainter(
-                data = "https://developer.android.com/images/brand/Android_Robot.png"
-            ),
-            contentDescription = "Android Logo",
-            modifier = Modifier.size(50.dp)
-        )
-
-        Spacer(modifier = Modifier.width(50.dp))
-        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
-    }
-}
-
-@Composable
-fun ScrollingList() {
-    val listSize = 100
-    // We save the scrolling position with this state
-    val scrollState = rememberLazyListState()
-    // We save the coroutine where our animated scroll will be executed
-    val coroutineScope = rememberCoroutineScope()
-
-    Column {
-        Row {
-            Button(onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollToItem(0)
-                }
-            },
-                modifier = Modifier.weight(1f)) {
-                Text("Scroll to the top")
-            }
-
-            Button(onClick = {
-                coroutineScope.launch {
-                    scrollState.animateScrollToItem(listSize-1)
-                }
-            }, modifier = Modifier.weight(1f)) {
-                Text("Scroll to the end")
-            }
-        }
-
-        LazyColumn(state = scrollState) {
-            items(listSize) {
-                ImageListItem(index = it)
-            }
-        }
-    }
-}
-
-@Preview
-@Composable
-fun SimpleListPreview() {
-    LayoutsCodelabTheme {
-        ImageList()
-    }
-}
+//@Composable
+//fun ImageList() {
+//    val scrollState = rememberLazyListState()
+//
+//    LazyColumn(state = scrollState) {
+//        items(100) {
+//            ImageListItem(it)
+//        }
+//    }
+//}
+//
+//@Composable
+//fun ImageListItem(index: Int) {
+//    Row(verticalAlignment = Alignment.CenterVertically) {
+//        Image(
+//            painter = rememberImagePainter(
+//                data = "https://developer.android.com/images/brand/Android_Robot.png"
+//            ),
+//            contentDescription = "Android Logo",
+//            modifier = Modifier.size(50.dp)
+//        )
+//
+//        Spacer(modifier = Modifier.width(50.dp))
+//        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+//    }
+//}
+//
+//@Composable
+//fun ScrollingList() {
+//    val listSize = 100
+//    // We save the scrolling position with this state
+//    val scrollState = rememberLazyListState()
+//    // We save the coroutine where our animated scroll will be executed
+//    val coroutineScope = rememberCoroutineScope()
+//
+//    Column {
+//        Row {
+//            Button(onClick = {
+//                coroutineScope.launch {
+//                    scrollState.animateScrollToItem(0)
+//                }
+//            },
+//                modifier = Modifier.weight(1f)) {
+//                Text("Scroll to the top")
+//            }
+//
+//            Button(onClick = {
+//                coroutineScope.launch {
+//                    scrollState.animateScrollToItem(listSize-1)
+//                }
+//            }, modifier = Modifier.weight(1f)) {
+//                Text("Scroll to the end")
+//            }
+//        }
+//
+//        LazyColumn(state = scrollState) {
+//            items(listSize) {
+//                ImageListItem(index = it)
+//            }
+//        }
+//    }
+//}
+//
+//@Preview
+//@Composable
+//fun SimpleListPreview() {
+//    LayoutsCodelabTheme {
+//        ImageList()
+//    }
+//}
 
 //
 //@Composable
